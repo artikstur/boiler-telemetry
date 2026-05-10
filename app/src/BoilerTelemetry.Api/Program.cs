c7d7a211758812e5502e7d1a0e1032915fc4b32c
Гурин Артур <arturgurin39@gmail.com>
2026-05-10 12:32:25 +0300

    включил сваггер

--- app/src/BoilerTelemetry.Api/Program.cs
+++ app/src/BoilerTelemetry.Api/Program.cs
@@ -89,11 +89,12 @@ app.UseSerilogRequestLogging(opts =>
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Boiler Telemetry API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseRouting();
 app.UseHttpMetrics();   // длительность/количество запросов по маршрутам
